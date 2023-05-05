e8152eb06cec649b9804b1dec8480adf5adb525d
MiaoMint <1981324730@qq.com>
2023-05-06 00:13:36 +0800

    feat: 状态栏沉浸

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:miru_app/pages/detail/index.dart';
 import 'package:miru_app/pages/main/index.dart';
 
 void main() {
   runApp(const MainApp());
+  if (Platform.isAndroid) {
+    SystemUiOverlayStyle style = const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark);
+    SystemChrome.setSystemUIOverlayStyle(style);
+  }
 }
 
 class MainApp extends StatelessWidget {
