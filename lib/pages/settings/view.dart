f105c4f977e6d392b42b00f97ae01717a99256a6
MiaoMint <1981324730@qq.com>
2023-07-06 15:01:55 +0800

    feat: `关于` 设置

--- lib/pages/settings/view.dart
+++ lib/pages/settings/view.dart
@@ -11,7 +11,10 @@ import 'package:miru_app/pages/settings/widgets/setting_switch_tile.dart';
 import 'package:miru_app/pages/settings/widgets/setting_tile.dart';
 import 'package:miru_app/utils/miru_storage.dart';
 import 'package:miru_app/utils/package_info.dart';
+import 'package:miru_app/widgets/button.dart';
+import 'package:miru_app/widgets/list_title.dart';
 import 'package:miru_app/widgets/platform_widget.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -126,18 +129,78 @@ class _SettingsPageState extends State<SettingsPage> {
         ),
         const SizedBox(height: 8),
         if (!Platform.isAndroid)
-          Obx(() {
-            final value = c.extensionLogWindowId.value != -1;
-            return SettingSwitchTile(
-              icon: const Icon(fluent.FluentIcons.bug),
-              title: "扩展日志窗口",
-              buildSubtitle: () => "用于调试扩展",
-              buildValue: () => value,
-              onChanged: (value) {
-                c.toggleExtensionLogWindow(value);
-              },
-            );
-          }),
+          Obx(
+            () {
+              final value = c.extensionLogWindowId.value != -1;
+              return SettingSwitchTile(
+                icon: const Icon(fluent.FluentIcons.bug),
+                title: "扩展日志窗口",
+                buildSubtitle: () => "用于调试扩展",
+                buildValue: () => value,
+                onChanged: (value) {
+                  c.toggleExtensionLogWindow(value);
+                },
+              );
+            },
+          ),
+        const SizedBox(height: 8),
+        const ListTitle(title: "关于"),
+        const SizedBox(height: 8),
+        SettingTile(
+          icon: const PlatformWidget(
+            androidWidget: Icon(Icons.web),
+            desktopWidget: Icon(fluent.FluentIcons.live_site, size: 24),
+          ),
+          title: '官方网站',
+          buildSubtitle: () => 'https://miru.js.org',
+          trailing: PlatformTextButton(
+            child: const Text('打开官网'),
+            onPressed: () {
+              launchUrl(
+                Uri.parse('https://miru.js.org'),
+                mode: LaunchMode.externalApplication,
+              );
+            },
+          ),
+        ),
+        const SizedBox(height: 8),
+        SettingTile(
+          icon: const PlatformWidget(
+            androidWidget: Icon(Icons.code),
+            desktopWidget: Icon(fluent.FluentIcons.code, size: 24),
+          ),
+          title: '开源',
+          buildSubtitle: () => 'miru-project/miru-app',
+          trailing: PlatformTextButton(
+            child: const Text('前往 Star'),
+            onPressed: () {
+              launchUrl(
+                Uri.parse('https://github.com/miru-project/miru-app'),
+                mode: LaunchMode.externalApplication,
+              );
+            },
+          ),
+        ),
+        if (Platform.isAndroid) ...[
+          const SizedBox(height: 8),
+          SettingTile(
+            icon: const Icon(Icons.library_books),
+            title: '许可',
+            buildSubtitle: () => '许可证',
+            trailing: const Icon(Icons.chevron_right),
+            onTap: () {
+              Get.to(LicensePage(
+                applicationIcon: const ImageIcon(
+                  AssetImage('assets/icon/logo.png'),
+                  size: 120,
+                ),
+                applicationName: "Miru",
+                applicationVersion: packageInfo.version,
+                applicationLegalese: "AGPLv3 © 2023 MiruProject.",
+              ));
+            },
+          )
+        ]
       ],
     );
   }
