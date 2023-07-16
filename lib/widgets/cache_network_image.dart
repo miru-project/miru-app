b0bf16130650ae20ea68adc9a27ffcffcadc434c
MiaoMint <1981324730@qq.com>
2023-07-16 22:36:45 +0800

    pref: 优化图片显示

--- lib/widgets/cache_network_image.dart
+++ lib/widgets/cache_network_image.dart
@@ -1,15 +1,8 @@
-import 'dart:io';
-
-import 'package:dio/dio.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fluent_ui/fluent_ui.dart' as fluent;
 import 'package:flutter/material.dart';
-import 'package:miru_app/utils/miru_directory.dart';
-import 'package:miru_app/widgets/progress_ring.dart';
-import 'package:path/path.dart' as path;
 
-final Dio _dio = Dio();
-
-class CacheNetWorkImage extends StatefulWidget {
+class CacheNetWorkImage extends StatelessWidget {
   const CacheNetWorkImage(
     this.url, {
     Key? key,
@@ -24,73 +17,21 @@ class CacheNetWorkImage extends StatefulWidget {
   final double? height;
   final Widget? fallback;
 
-  @override
-  State<CacheNetWorkImage> createState() => _CacheNetWorkImageState();
-}
-
-class _CacheNetWorkImageState extends State<CacheNetWorkImage> {
-  bool isloading = true;
-  bool isError = false;
-  String fileName = '';
-
-  @override
-  void initState() {
-    _init();
-    super.initState();
-  }
-
-  _init() async {
-    final filePath =
-        path.join(await MiruDirectory.getCacheDirectory, 'image_cache');
-    Directory(filePath).createSync(recursive: true);
-    fileName =
-        path.join(filePath, widget.url.replaceAll(RegExp(r'[\\/:*?"<>|]'), ''));
-    if (File(fileName).existsSync()) {
-      setState(() {
-        isloading = false;
-      });
-      return;
-    }
-    try {
-      await _dio.download(widget.url, fileName);
-      isloading = false;
-    } catch (e) {
-      debugPrint(e.toString());
-      isError = true;
-    } finally {
-      if (mounted) {
-        setState(() {});
-      }
-    }
-  }
-
   _errorBuild() {
-    if (widget.fallback != null) {
-      return widget.fallback!;
+    if (fallback != null) {
+      return fallback!;
     }
     return const Center(child: Icon(fluent.FluentIcons.error));
   }
 
   @override
   Widget build(BuildContext context) {
-    if (isError) {
-      return _errorBuild();
-    }
-    if (isloading) {
-      return const Center(
-        child: SizedBox(
-          width: 25,
-          height: 25,
-          child: ProgressRing(),
-        ),
-      );
-    }
-    return Image.file(
-      File(fileName),
-      fit: widget.fit,
-      width: widget.width,
-      height: widget.height,
-      errorBuilder: (context, error, stackTrace) => _errorBuild(),
+    return CachedNetworkImage(
+      imageUrl: url,
+      fit: fit,
+      width: width,
+      height: height,
+      errorWidget: (context, url, error) => _errorBuild(),
     );
   }
 }
