e778cdb500242462d2772c1a8a71323df03fdb80
MiaoMint <miaomint@0u0.ren>
2024-01-09 00:13:02 +0800

    refactor: settings page

--- lib/controllers/settings_controller.dart
+++ lib/controllers/settings_controller.dart
@@ -2,10 +2,24 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:desktop_multi_window/desktop_multi_window.dart';
+import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
 class SettingsController extends GetxController {
+  final contributors = [].obs;
   final extensionLogWindowId = (-1).obs;
+
+  final links = {
+    'Github': 'https://github.com/miru-project/miru-app',
+    'Telegram Group': 'https://t.me/MiruChat',
+    'Website': 'https://miru.js.org',
+  };
+
+  @override
+  void onInit() {
+    super.onInit();
+    _getContributors();
+  }
 
   void toggleExtensionLogWindow(bool open) async {
     if (open && extensionLogWindowId.value == -1) {
@@ -34,5 +48,13 @@ class SettingsController extends GetxController {
     }
     WindowController.fromWindowId(extensionLogWindowId.value).close();
     extensionLogWindowId.value = -1;
+  }
+
+  _getContributors() async {
+    final res = await Dio()
+        .get("https://api.github.com/repos/miru-project/miru-app/contributors");
+    contributors.value = List.from(res.data)
+        .where((element) => element["type"] == "User")
+        .toList();
   }
 }
---
+++ lib/views/widgets/settings/settings_expander_tile.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart' as fluent;
+import 'package:get/get.dart';
+import 'package:miru_app/views/widgets/platform_widget.dart';
+import 'package:miru_app/views/widgets/settings/settings_tile.dart';
+
+class SettingsExpanderTile extends StatelessWidget {
+  const SettingsExpanderTile({
+    super.key,
+    this.icon,
+    this.androidIcon,
+    this.leading,
+    required this.content,
+    required this.title,
+    required this.subTitle,
+    this.open = false,
+    this.noPage = false,
+  });
+  final IconData? icon;
+  final IconData? androidIcon;
+  final Widget? leading;
+  final String title;
+  final String subTitle;
+  final bool open;
+  final Widget content;
+  // 不使用二级页面
+  final bool noPage;
+
+  Widget _buildAndroid(BuildContext context) {
+    if (noPage) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 10),
+            Text(
+              title,
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 2),
+            Text(
+              subTitle,
+              style: const TextStyle(fontSize: 12),
+            ),
+            const SizedBox(height: 15),
+            content,
+          ],
+        ),
+      );
+    }
+
+    Widget iconWidget = androidIcon != null
+        ? Icon(androidIcon, size: 24)
+        : icon != null
+            ? Icon(icon, size: 24)
+            : leading!;
+
+    return SettingsTile(
+      icon: iconWidget,
+      title: title,
+      buildSubtitle: () => subTitle,
+      trailing: const Icon(Icons.chevron_right),
+      onTap: () {
+        Get.to(
+          Scaffold(
+            appBar: AppBar(
+              title: Text(title),
+            ),
+            body: content,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildDesktop(BuildContext context) {
+    return fluent.Expander(
+      initiallyExpanded: open,
+      leading: icon != null ? Icon(icon, size: 24) : leading,
+      header: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 10),
+          Text(title),
+          const SizedBox(height: 2),
+          Text(
+            subTitle,
+            style: const TextStyle(fontSize: 12),
+          ),
+          const SizedBox(height: 15)
+        ],
+      ),
+      content: content,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformBuildWidget(
+      androidBuilder: _buildAndroid,
+      desktopBuilder: _buildDesktop,
+    );
+  }
+}
